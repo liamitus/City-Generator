fddc67161a0d796f7b588abfcf90da22d18bd559
Liam Howell <liammm@gmail.com>
2021-09-09 07:14:52 -0400

    Rubber band box selection working

--- Assets/Scripts/UnitSelection.cs
+++ Assets/Scripts/UnitSelection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -36,12 +37,12 @@ public class UnitSelection : MonoBehaviour {
 
         // Left mouse button release
         if (Input.GetMouseButtonUp(0)) {
-
+            ReleaseSelectionBox();
         }
 
     }
 
-    void TrySelect(Vector2 screenPos) {
+    private void TrySelect(Vector2 screenPos) {
         Ray ray = cam.ScreenPointToRay(screenPos);
         RaycastHit hit;
 
@@ -56,7 +57,7 @@ public class UnitSelection : MonoBehaviour {
     }
 
     // Called when we are creating a selection box
-    void UpdateSelectionBox(Vector2 currentMousePosition) {
+    private void UpdateSelectionBox(Vector2 currentMousePosition) {
         if (!selectionBox.gameObject.activeInHierarchy) {
             selectionBox.gameObject.SetActive(true);
         }
@@ -68,7 +69,26 @@ public class UnitSelection : MonoBehaviour {
         selectionBox.anchoredPosition = startPos + new Vector2(width / 2, height / 2);
     }
 
-    void ToggleSelectionVisual(bool selected) {
+    // Called when we release the selection box
+    private void ReleaseSelectionBox() {
+        selectionBox.gameObject.SetActive(false);
+
+        Vector2 min = selectionBox.anchoredPosition - (selectionBox.sizeDelta / 2);
+        Vector2 max = selectionBox.anchoredPosition + (selectionBox.sizeDelta / 2);
+
+        foreach (Unit unit in player.units) {
+            Vector3 screenPosition = cam.WorldToScreenPoint(unit.transform.position);
+            if (
+                screenPosition.x > min.x && screenPosition.x < max.x
+                && screenPosition.y > min.y && screenPosition.y < max.y
+            ) {
+                selectedUnits.Add(unit);
+                unit.ToggleSelectionVisual(true);
+            }
+        }
+    }
+
+    private void ToggleSelectionVisual(bool selected) {
         foreach (Unit unit in selectedUnits) {
             unit.ToggleSelectionVisual(selected);
         }
